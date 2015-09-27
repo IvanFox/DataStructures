e14b4e389f66afe6789294b1c670c02fde98c49f
Ivan Lissitsnoi <ivanfox@protonmail.ch>
2015-09-27 12:24:45 +0100

    Stack implementation added

---
+++ stack/MyStack.java
@@ -0,0 +1,49 @@
+package stack;
+
+/**
+ * Created by ivan on 27/09/15.
+ */
+public class MyStack <T> {
+    T[] items;
+    int top;
+    final int INITIAL_SIZE = 10;
+
+
+    public MyStack(){
+        items = (T[]) new Object[INITIAL_SIZE];
+        top = 0;
+    }
+
+    public boolean isEmpty(){
+        return top == 0;
+    }
+
+    public int getSize(){
+        return top;
+    }
+
+    public void push(T el){
+        increaseSize();
+        items[top++] = el;
+    }
+
+    public T pop(){
+        if(!isEmpty())
+            return items[--top];
+        return null;
+    }
+
+    public void increaseSize(){
+        if(top > items.length / 2) {
+            T temp[] = items;
+            items = (T[]) new Object[temp.length * 2];
+
+            int i = 0;
+            while (temp[i] != null) {
+                items[i] = temp[i];
+                i++;
+            }
+            temp = null;
+        }
+    }
+}
