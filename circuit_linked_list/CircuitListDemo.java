26c7c681635a76d6c2c0850c728495ffbc497bed
IvanFox <zvezdo4eet@gmail.com>
2015-10-21 18:19:49 +0100

    Circuit Linked List added

---
+++ circuit_linked_list/CircuitListDemo.java
@@ -0,0 +1,26 @@
+package DataStructures.circuit_linked_list;
+
+/**
+ * Created by IvanLis on 21/10/15.
+ * Student No: c00185055
+ * All rights reserved
+ */
+
+public class CircuitListDemo {
+
+    public static void main(String[] args) {
+        CircuitLinkedList<Integer> circuitLinkedList = new CircuitLinkedList<>();
+
+        circuitLinkedList.insertNewLink(new Link(43));
+        circuitLinkedList.insertNewLink(new Link(3));
+        circuitLinkedList.insertNewLink(new Link(23));
+        circuitLinkedList.insertNewLink(new Link(13));
+        circuitLinkedList.insertNewLink(new Link(13));
+        circuitLinkedList.insertNewLink(new Link(10));
+        circuitLinkedList.display();
+        System.out.println("--------------------\n");
+        circuitLinkedList.removeLink(10);
+        circuitLinkedList.display();
+
+    }
+}
