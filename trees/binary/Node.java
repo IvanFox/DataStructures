d5963c35b4afb6304c25fb74a518dfdb6fa94ebd
IvanFox <zvezdo4eet@gmail.com>
2015-11-08 14:37:31 +0000

    init trees package

---
+++ trees/binary/Node.java
@@ -0,0 +1,9 @@
+package DataStructures.trees.binary;
+
+/**
+ * Created by IvanLis on 08/11/15.
+ * Student No: c00185055
+ * All rights reserved
+ */
+public class Node {
+}
---
+++ trees/heap/Node.java
@@ -0,0 +1,18 @@
+package DataStructures.trees.heap;
+
+/**
+ * Created by IvanLis on 08/11/15.
+ * Student No: c00185055
+ * All rights reserved
+ */
+public class Node <T> {
+
+    private T data;
+    private Node lChild;
+    private Node rChild;
+
+
+    
+
+
+}
