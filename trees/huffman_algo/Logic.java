c69b8986991661cc415facf0e7356cf1c7bbc7ca
IvanFox <zvezdo4eet@gmail.com>
2015-11-16 12:58:11 +0000

    minor refactoring

--- trees/huffman_algo/Logic.java
+++ trees/huffman_algo/Logic.java
@@ -1,5 +1,7 @@
 package DataStructures.trees.huffman_algo;
 
+import DataStructures.trees.binary.BinaryTree;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
@@ -13,13 +15,13 @@ import java.util.Map;
  */
 public class Logic {
 
-    private static int TOTAL_SIZE = 0;
+    private static int TOTAL_SIZE = 0; // Hold total length of a text doc
 
     private static HashMap<Character, Double> readFile(String filename, HashMap<Character, Double> hashMap){
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
             double c, value;
             while ((c = bufferedReader.read()) != -1) {
-                // if current char is not in exist as a value in HashMap -> add it
+                // if current char is not exist as a value in HashMap -> add it
                 if(hashMap.get((char)c) == null) {
                     hashMap.put((char) c, Double.valueOf(1));
                 }
@@ -42,9 +44,9 @@ public class Logic {
             hashMap.put(entry.getKey(), value);
         }
         return hashMap;
-
-
     }
+
+
 
     public static void main(String[] args) {
         String filename = "/Users/IvanLis/test.txt";
@@ -53,11 +55,14 @@ public class Logic {
         hashMap = readFile(filename,hashMap);
 
         hashMap.forEach((k, v) -> System.out.println(k + " = " + v));
-        System.out.println();
         System.out.println(TOTAL_SIZE);
-
         hashMap = findOccurrence(hashMap);
         hashMap.forEach((k, v) -> System.out.println(k + " = " + v));
+
+
+
+
+
 
 
 
