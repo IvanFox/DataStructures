988dd4aad7155aa506b6786f64b0d4f4f238fd04
IvanFox <zvezdo4eet@gmail.com>
2015-11-20 19:49:43 +0000

    some testing added

--- trees/huffman_algo/Demo.java
+++ trees/huffman_algo/Demo.java
@@ -11,9 +11,22 @@ public class Demo {
     public static void main(String[] args) throws Exception {
 
         Logic logic = new Logic("/Users/IvanLis/test.txt");
+        logic.getHashMap().forEach((key, value) -> System.out.println("Character: " + key + "\tOccurrence: "  + value));
         logic.getHuffmanCodes().forEach((k,v) -> System.out.println("Character: " + k  + "\tCode: " + v));
         System.out.println("\nEncoded output:");
         System.out.println(logic.generateEncodedOutput());
+        System.out.println("\nDecoded output");
+        System.out.println(logic.decodeEncodedOutput(logic.generateEncodedOutput()));
+
+
+
+//        StringBuilder builder = new StringBuilder();
+
+
+//        System.out.println(builder.length());
+//        builder.append("a");
+//        System.out.println(builder.deleteCharAt(0));
+//        System.out.println(builder.length());
 
     }
 }
