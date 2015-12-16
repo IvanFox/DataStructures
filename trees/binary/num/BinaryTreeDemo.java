ae83c947f22461815ad128558d694763909e8dab
IvanFox <zvezdo4eet@gmail.com>
2015-12-16 21:37:17 +0000

    some testing added

--- trees/binary/num/BinaryTreeDemo.java
+++ trees/binary/num/BinaryTreeDemo.java
@@ -17,6 +17,26 @@ public class BinaryTreeDemo {
         root = avlTree.addNode(root, 4);
         root = avlTree.addNode(root, 3);
         root = avlTree.addNode(root, 2);
+        root = avlTree.addNode(root, 1);
+        root = avlTree.addNode(root, 6);
+        root = avlTree.addNode(root, 8);
+        root = avlTree.addNode(root, 0);
+        root = avlTree.addNode(root, 5);
+        root = avlTree.addNode(root, 9);
+        root = avlTree.addNode(root, 15);
+        root = avlTree.addNode(root, 45);
+        root = avlTree.addNode(root, 10);
+        root = avlTree.addNode(root, 34);
+        root = avlTree.addNode(root, 40);
+        root = avlTree.addNode(root, 45);
+        root = avlTree.addNode(root, 50);
+        root = avlTree.addNode(root, 55);
+        root = avlTree.addNode(root, 55);
+        root = avlTree.addNode(root, 65);
+        root = avlTree.addNode(root, 75);
+        root = avlTree.addNode(root, 85);
+        root = avlTree.addNode(root, 95);
+
 
         System.out.println("Pre Order");
         avlTree.preOrder(root);
@@ -24,11 +44,9 @@ public class BinaryTreeDemo {
         System.out.println("\nIn Order");
         avlTree.inOrder(root);
 
-        System.out.println("-----------------------------");
-        avlTree.findMax(root);
+        System.out.println("Root value: " + root.getValue());
 
-        System.out.println("-----------------------------");
-        avlTree.findMin(root);
 
+//        avlTree.findMinMax(root);
     }
 }
