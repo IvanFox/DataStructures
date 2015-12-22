a4f3efc974549f024670129168ea0d47fe59ee4d
IvanFox <zvezdo4eet@gmail.com>
2015-12-22 16:03:55 +0000

    init commit

---
+++ graphs/GraphAdjMatrix.java
@@ -0,0 +1,43 @@
+package dataStructures.graphs;
+
+import java.util.List;
+
+/**
+ * @author IvanLis
+ * @version 1.0
+ * @since 22/12/2015.
+ * <p>
+ * labs
+ */
+public class GraphAdjMatrix extends Graph {
+
+    private int[][] adjMatrix;
+    
+    @Override
+    public void implementAddVertex() {
+        int v = getNumVerticies();
+        if (v >= adjMatrix.length) {
+            int[][] newAdjMatrix = new int[v*2][v*2];
+
+            for (int i = 0; i < adjMatrix.length; i++) {
+                for (int j = 0; j < adjMatrix.length; j++) {
+                    newAdjMatrix[i][j] = adjMatrix[i][j];
+                }
+            }
+            adjMatrix = newAdjMatrix;
+        }
+        for (int i = 0; i < adjMatrix[v].length ; i++) {
+            adjMatrix[v][i] = 0;
+        }
+    }
+
+    @Override
+    public void implementAddEdge(int v, int w) {
+        adjMatrix[v][w] = 1;
+    }
+
+    @Override
+    public List<Integer> getNeighbors(int v) {
+        return null;
+    }
+}
