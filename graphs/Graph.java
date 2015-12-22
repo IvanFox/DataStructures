2a8692ae3188fa498ca51d8405bf72ccb58e1d72
IvanFox <zvezdo4eet@gmail.com>
2015-12-22 22:23:21 +0000

    reformat

--- graphs/Graph.java
+++ graphs/Graph.java
@@ -116,8 +116,7 @@ public abstract class Graph {
     public List<Integer> degreeSequence() {
         List<Integer> degree = new ArrayList<>();
         for (int i = 0; i < getNumVertices(); i++) {
-            int sum = Integer.sum(getNeighbors(i).size(), getInNeighbors(i).size());
-            degree.add(sum);
+            degree.add(getNeighbors(i).size());
         }
         degree = degree.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         return degree;
