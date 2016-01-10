06e636ec30dbaeb3eb49b3a365ba996c367e9516
IvanFox <zvezdo4eet@gmail.com>
2016-01-10 12:14:36 +0000

    reformat

--- list/MyList.java
+++ list/MyList.java
@@ -1,41 +1,40 @@
 package dataStructures.list;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 /**
  * Created by ivan on 24/09/15.
  * Implementation of a List using generic type of a class
  */
-public class MyList <T> implements Iterable<T>{
+public class MyList<T> implements Iterable<T> {
 
     private T items[];
     private int count;
     final int INITIAL_SIZE = 5;
 
-    public MyList(){
+    public MyList() {
         items = (T[]) new Object[INITIAL_SIZE];
         count = 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return count;
     }
 
-    private boolean checkRange(int index){
+    private boolean checkRange(int index) {
         return index <= count && index >= 0;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return count == 0;
     }
 
-    public boolean isFull(){
+    public boolean isFull() {
         return count == items.length;
     }
 
-    public T set(int index, T el){
-        if (checkRange(index)){
+    public T set(int index, T el) {
+        if (checkRange(index)) {
             T temp = items[index];
             items[index] = el;
             return temp;
@@ -44,19 +43,19 @@ public class MyList <T> implements Iterable<T>{
         return null;
     }
 
-    public T get(int index){
-        if (checkRange(index) && !isEmpty()){
+    public T get(int index) {
+        if (checkRange(index) && !isEmpty()) {
             return items[index];
         }
         System.out.println("index " + index + ": not exist");
         return null;
     }
 
-    public void add(int index, T el){
+    public void add(int index, T el) {
         increaseSize();
-        if (checkRange(index)){
-            for (int i = count; i > index ; i--) {
-                items[i] = items[i-1];
+        if (checkRange(index)) {
+            for (int i = count; i > index; i--) {
+                items[i] = items[i - 1];
             }
 
             items[index] = el;
@@ -65,16 +64,14 @@ public class MyList <T> implements Iterable<T>{
     }
 
     // add new element to the end
-    public void add(T el){
-        increaseSize();
-        if (count != items.length)
-            items[count++] = el;
-        else
-            System.out.println("List is full");
+    public void add(T el) {
+        if (isFull())
+            increaseSize();
+        items[count++] = el;
     }
 
-    public T remove(int index){
-        if(checkRange(index) && !isEmpty()){
+    public T remove(int index) {
+        if (checkRange(index) && !isEmpty()) {
             T temp = items[index];
             for (int i = index; i < count; i++) {
                 items[i] = items[i + 1];
@@ -86,33 +83,25 @@ public class MyList <T> implements Iterable<T>{
         return null;
     }
 
-    public int indexOf(T el){
+    public int indexOf(T el) {
         int sentinel = 0;
         while (sentinel != count && items[sentinel] != el) {
-           sentinel++;
+            sentinel++;
         }
-        if (sentinel == count){
+        if (sentinel == count) {
             return -1;
         }
         return sentinel;
     }
 
-    public void increaseSize(){
-        if(count > items.length / 2) {
-            T temp[] = items;
-            items = (T[]) new Object[temp.length * 2];
+    public void increaseSize() {
+        T temp[] = items;
+        items = (T[]) new Object[temp.length * 2];
 
-            int i = 0;
-            while (temp[i] != null) {
-                items[i] = temp[i];
-                i++;
-            }
-        }
-    }
-
-    public void printElements(){
-        for (int i = 0; i < count; i++) {
-            System.out.println("[ " + items[i].toString() + " ]");
+        int i = 0;
+        while (temp[i] != null) {
+            items[i] = temp[i];
+            i++;
         }
     }
 
