16759566afd6dca4ea76486d6e0b9407b1c76950
IvanFox <zvezdo4eet@gmail.com>
2016-01-14 21:35:29 +0000

    some testing added

--- list/Demo.java
+++ list/Demo.java
@@ -11,12 +11,20 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        ListImpl<Integer> list = new ListImpl<>();
+        ListImpl<Double> list = new ListImpl<>();
 
         for (int i = 0; i < 10; i++) {
-            list.add((int)(Math.random() * 100));
+            list.add((Math.random() * 100));
         }
 
+        list.add(10.0);
+        list.add(11, 11.0);
+        list.remove(10.0);
+        System.out.println(list.size());
+        list.remove(10);
         list.forEach(System.out::println);
+
+        System.out.println("\n\n\n");
+        list.subList(3,5).forEach(System.out::println);
     }
 }
