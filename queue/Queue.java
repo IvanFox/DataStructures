127574ce715d39c124d9af8b09f4f241f7e80bfa
IvanFox <zvezdo4eet@gmail.com>
2016-01-27 18:43:59 +0000

    link class in moved inside Queue as an InnerClass

--- queue/Queue.java
+++ queue/Queue.java
@@ -55,4 +55,34 @@ public class Queue {
             theLink = theLink.getNext();
         }
     }
+
+    private class Link <T> {
+
+        private T el;
+
+        private Link next;
+
+        public Link(T el) {
+            this.el = el;
+            next = null;
+        }
+
+        public T getEl() {
+            return el;
+        }
+
+        public void setEl(T el) {
+            this.el = el;
+        }
+
+
+        public void setNext(Link next) {
+            this.next = next;
+        }
+
+        public Link getNext() {
+            return next;
+        }
+    }
+
 }
